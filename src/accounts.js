174b3954847ff0f8ad0707e3822aa383e4bd120b
J M Rossy <jm.rossy@gmail.com>
2020-02-20 14:17:46 +0100

    Implement the account and token commands using ContractKit (#1)

--- src/accounts.js
+++ src/accounts.js
@@ -1,11 +1,28 @@
 const web3 = require('web3')
+const fs = require('fs');
+
+const SECRET_PATH = '.secret'
+const web3Instance = new web3()
 
 function getAccount() {
   console.log('Getting your account')
+  if (!fs.existsSync(SECRET_PATH)) {
+    console.log('No account found, create one first')
+    return false
+  }
+
+  const privateKey = fs.readFileSync(SECRET_PATH, 'utf8');
+  const account = web3Instance.eth.accounts.privateKeyToAccount(privateKey);
+  console.log(`Found account ${account.address}`)
+  return account
 }
 
 function createAccount() {
   console.log('Creating a new account')
+  const account = web3Instance.eth.accounts.create();
+  console.log(`Made new account ${account.address}`)
+  fs.writeFileSync(SECRET_PATH, account.privateKey)
+  console.log(`Account private key saved to ${SECRET_PATH}`)
 }
 
 module.exports = {
--- src/tokens.js
+++ src/tokens.js
@@ -1,15 +1,53 @@
+const contractkit = require('@celo/contractkit')
+const accounts = require('./accounts')
 
+const NODE_URL = 'https://alfajores-forno.celo-testnet.org'
 
-function getBalances() {
+async function getBalances() {
   console.log('Getting your balances')
+  const kit = contractkit.newKit(NODE_URL)
+  const address = accounts.getAccount().address
+  const balances = await kit.getTotalBalance(address)
+  console.log(`Dollar balance: ${balances.usd}`)
+  console.log(`Gold balance: ${balances.gold}`)
+  kit.stop()
 }
 
-function sendPayment(params) {
+async function sendPayment(params) {
   const recipient = params.to
   const amount = params.amount
   const token = params.token
-
   console.log(`Sending payment of ${amount} ${token} to ${recipient}`)
+
+  const kit = contractkit.newKit(NODE_URL)
+  
+  // Set up your account in contract kit
+  const account = accounts.getAccount()
+  kit.addAccount(account.privateKey)
+  kit.defaultAccount = account.address
+  console.log('Kit account is set up')
+
+  // Get the right token contract
+  let contract
+  if (token.toLowerCase() === 'cusd') {
+    contract = await kit.contracts.getStableToken()
+  }
+  else if (token.toLowerCase() === 'cgld') {
+    contract = await kit.contracts.getGoldToken()
+  }
+  else {
+    console.error(`Invalid token ${token}, use cGLD or cUSD`)
+    return false
+  }
+  console.log('Kit contract is set up, creating transaction')
+
+  // Create the payment transaction
+  const tx = await contract.transfer(recipient, amount).send()
+  const receipt = await tx.waitReceipt()
+  console.log('Tx receipt recieved', receipt)
+  const newBalance = await contract.balanceOf(account.address)
+  console.log(`New balance is ${newBalance.toString()}`)
+  kit.stop()
 }
 
 module.exports = {
